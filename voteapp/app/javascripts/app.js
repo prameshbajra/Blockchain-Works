31ac53133e645eda743103fa1761c0f7180629be
Suzal3579 <pe.messh@gmail.com>
2018-01-04 21:35:02 +0530

    Time to migrate contracts ... Waiting for some testnet ethers ...

--- voteapp/app/javascripts/app.js
+++ voteapp/app/javascripts/app.js
@@ -2,101 +2,68 @@
 import "../stylesheets/app.css";
 
 // Import libraries we need.
-import { default as Web3} from 'web3';
+import { default as Web3 } from 'web3';
 import { default as contract } from 'truffle-contract'
 
-// Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+/*
+ * When you compile and deploy your Voting contract,
+ * truffle stores the abi and deployed address in a json
+ * file in the build directory. We will use this information
+ * to setup a Voting abstraction. We will use this abstraction
+ * later to create an instance of the Voting contract.
+ * Compare this against the index.js from our previous tutorial to see the difference
+ * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
+ */
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+import voting_artifacts from '../../build/contracts/Voting.json'
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
+var Voting = contract(voting_artifacts);
 
-window.App = {
-  start: function() {
-    var self = this;
+let candidates = { "Suzal": "candidate-1", "Pramesh": "candidate-2", "Bajra": "candidate-3" }
 
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
+window.voteForCandidate = function (candidate) {
+    let candidateName = $("#candidate").val();
+    try {
+        $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
+        $("#candidate").val("");
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
+        /* Voting.deployed() returns an instance of the contract. Every call
+         * in Truffle returns a promise which is why we have used then()
+         * everywhere we have a transaction call
+         */
+        Voting.deployed().then(function (contractInstance) {
+            contractInstance.voteForCandidate(candidateName, { gas: 140000, from: web3.eth.accounts[0] }).then(function () {
+                let div_id = candidates[candidateName];
+                return contractInstance.totalVotesFor.call(candidateName).then(function (v) {
+                    $("#" + div_id).html(v.toString());
+                    $("#msg").html("");
+                });
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
+$(document).ready(function () {
+    if (typeof web3 !== 'undefined') {
+        console.warn("Using web3 detected from external source like Metamask")
+        // Use Mist/MetaMask's provider
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    }
 
-      accounts = accs;
-      account = accounts[0];
-
-      self.refreshBalance();
-    });
-  },
-
-  setStatus: function(message) {
-    var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  refreshBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
-  },
-
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
-  }
-};
-
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-  }
-
-  App.start();
+    Voting.setProvider(web3.currentProvider);
+    let candidateNames = Object.keys(candidates);
+    for (var i = 0; i < candidateNames.length; i++) {
+        let name = candidateNames[i];
+        Voting.deployed().then(function (contractInstance) {
+            contractInstance.totalVotesFor.call(name).then(function (v) {
+                $("#" + candidates[name]).html(v.toString());
+            });
+        })
+    }
 });
--- voteapp/truffle.js
+++ voteapp/truffle.js
@@ -2,11 +2,12 @@
 require('babel-register')
 
 module.exports = {
-  networks: {
-    development: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '*' // Match any network id
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*', // Match any network id
+            gas: 500000
+        }
     }
-  }
 }
