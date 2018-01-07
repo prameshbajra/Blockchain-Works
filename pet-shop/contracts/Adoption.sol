fec42758530d7f7695546ef693897de03fa7da00
Suzal3579 <pe.messh@gmail.com>
2018-01-07 14:41:06 +0530

    Finally working ....

--- pet-shop/contracts/Adoption.sol
+++ pet-shop/contracts/Adoption.sol
@@ -2,11 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
     address[16] public adopters;
-
     // Adopting a pet
     function adopt(uint petId) public returns(uint) {
         require(petId >= 0 && petId <= 15);
+
         adopters[petId] = msg.sender;
+
         return petId;
     }
     // Retrieving the adopters
--- pet-shop/src/js/app.js
+++ pet-shop/src/js/app.js
@@ -24,13 +24,16 @@ App = {
     },
 
     initWeb3: function () {
+        // Is there an injected web3 instance?
         if (typeof web3 !== 'undefined') {
             App.web3Provider = web3.currentProvider;
         } else {
             // If no injected web3 instance is detected, fall back to Ganache
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
         }
-        web3 = new Web3(App.web3Provider);
+        web3 = new Web3(App.web3Provider)
+
+        return App.initContract();
     },
 
     initContract: function () {
@@ -45,6 +48,7 @@ App = {
             // Use our contract to retrieve and mark the adopted pets
             return App.markAdopted();
         });
+        return App.bindEvents();
     },
 
     bindEvents: function () {
@@ -71,15 +75,21 @@ App = {
 
     handleAdopt: function (event) {
         event.preventDefault();
+
         var petId = parseInt($(event.target).data('id'));
+
         var adoptionInstance;
+
         web3.eth.getAccounts(function (error, accounts) {
             if (error) {
                 console.log(error);
             }
+
             var account = accounts[0];
+
             App.contracts.Adoption.deployed().then(function (instance) {
                 adoptionInstance = instance;
+
                 // Execute adopt as a transaction by sending account
                 return adoptionInstance.adopt(petId, { from: account });
             }).then(function (result) {
