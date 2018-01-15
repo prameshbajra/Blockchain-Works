89f5159558feb698327f1651eec878f87c65d052
Suzal3579 <pe.messh@gmail.com>
2018-01-15 16:40:27 +0530

    Successful migration to ropsten .... Working fine on both ropsten and localhost ...
    Happy moment. Git for the rescue.

--- letsvote/truffle.js
+++ letsvote/truffle.js
@@ -1,11 +1,19 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const infura_apikey = "aDwmNJ8VINV7YZku7EZe";
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
     networks: {
         development: {
-            host: "127.0.0.1",
+            host: "localhost",
             port: 8545,
-            network_id: "*"
+            network_id: "*" // Match any network id
+        },
+        ropsten: {
+            provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey),
+            network_id: 3,
+            gas: 3000000
         }
     }
 };
