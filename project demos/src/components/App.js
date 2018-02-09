8a5f9489b0fd9bf588f9cd92b661ed65efb656bc
Suzal3579 <pe.messh@gmail.com>
2018-02-09 15:54:32 +0530

    leaving this at some alter error ... JSON RPC error ...

--- project demos/src/components/App.js
+++ project demos/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../../build/contracts/SimpleStorage.json'
+import SimpleDemoContract from '../../build/contracts/SimpleDemo.json'
 import getWeb3 from '../utils/getWeb3'
 
 import '../css/oswald.css'
@@ -20,7 +20,6 @@ class App extends Component {
             this.setState({
                 web3: results.web3
             })
-            this.instantiateContract()
         }).catch(() => {
             console.log('Error finding web3.')
         })
@@ -28,19 +27,22 @@ class App extends Component {
 
     instantiateContract() {
         const contract = require('truffle-contract')
-        const simpleStorage = contract(SimpleStorageContract)
-        simpleStorage.setProvider(this.state.web3.currentProvider)
-        var simpleStorageInstance;
+        const simpleDemo = contract(SimpleDemoContract)
+        simpleDemo.setProvider(this.state.web3.currentProvider)
+        var simpleDemoInstance;
         // Get accounts.
         this.state.web3.eth.getAccounts((error, accounts) => {
-            simpleStorage.deployed().then((instance) => {
-                simpleStorageInstance = instance
+            simpleDemo.deployed().then((instance) => {
+                simpleDemoInstance = instance
                 // Stores a given value, 9 by default.
-                return simpleStorageInstance.set(9, { from: accounts[0] })
+                console.log(accounts);
+                // Getting error here ... This line ...
+                return simpleDemoInstance.getBalance(accounts[0], { from: accounts[0] })
+            }).then((returnedResults) => {
+                // console.log(returnedResults)
+                return simpleDemoInstance.getBalance(accounts[0], { from: accounts[0] })
             }).then((result) => {
-                // Get the value from the contract to prove it worked.
-                return simpleStorageInstance.get.call(accounts[0])
-            }).then((result) => {
+                console.log(result);
                 // Update state with the result.
                 return this.setState({ storageValue: result.c[0] })
             })
@@ -52,7 +54,7 @@ class App extends Component {
             <div className="container">
                 <h1 className="text-center">Good to Go!</h1>
                 <h3 className="text-center">
-                    {this.state.storageValue}
+                    <button onClick={(e) => { this.instantiateContract() }}>Send</button>
                 </h3>
             </div>
         );
