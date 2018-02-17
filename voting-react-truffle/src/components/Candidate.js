4c909b1959865b725029587fe9f0e4ff1712afad
Suzal3579 <pe.messh@gmail.com>
2018-02-17 14:28:45 +0530

    Working, writing data setVoterDetails ...

--- voting-react-truffle/src/components/Candidate.js
+++ voting-react-truffle/src/components/Candidate.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
-import getWeb3 from '../utils/getWeb3';
-import CandidateContract from '../../build/contracts/CandidateContract.json'
+// import getWeb3 from '../utils/getWeb3';
+// import CandidateContract from '../../build/contracts/CandidateContract.json'
 
 class Candidate extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div>
--- voting-react-truffle/src/components/Home.js
+++ voting-react-truffle/src/components/Home.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 import Candidate from './Candidate';
 import Voters from './Voters';
 
-import getWeb3 from '../utils/getWeb3';
-import VotingContract from '../../build/contracts/VotingContract.json'
+// import getWeb3 from '../utils/getWeb3';
+// import VotingContract from '../../build/contracts/VotingContract.json'
 
 class Home extends Component {
     render() {
