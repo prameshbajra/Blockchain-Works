007bce3fb43fd729c4b8325f8e223b98f915de10
Suzal3579 <pe.messh@gmail.com>
2018-02-21 13:54:35 +0530

    Fetching candidates working well. Reset garrda ni challdai xa ....

--- election-demo/src/components/App.js
+++ election-demo/src/components/App.js
@@ -10,7 +10,8 @@ class App extends Component {
         this.state = {
             web3: null,
             candidatesCount: undefined,
-            isButtonVisible: false
+            isButtonVisible: false,
+            candidateArray: [],
         }
     }
 
@@ -31,23 +32,39 @@ class App extends Component {
         const contract = require('truffle-contract');
         const electionContract = contract(Election);
         electionContract.setProvider(this.state.web3.currentProvider);
-        // Get accounts.
         this.state.web3.eth.getAccounts((error, accounts) => {
             electionContract.deployed().then((instance) => {
                 electionContractInstance = instance
-                // Stores a given value, 9 by default.
                 return electionContractInstance.addCandidate(candidateName, { from: accounts[0] })
             }).then((result) => {
-                // Get the value from the contract to prove it worked.
-                console.log("result", result);
                 return electionContractInstance.candidatesCount.call(accounts[0])
             }).then((result2) => {
-                // Update state with the result.
-                console.log("result2", result2);
                 this.setState({ candidatesCount: result2.c[0], isButtonVisible: true });
             }).catch((error) => {
                 console.log("You are not allowed to do this, Sorry !")
             });
+        })
+    }
+
+    getAllCandidates = () => {
+        let electionContractInstance;
+        const contract = require('truffle-contract');
+        const electionContract = contract(Election);
+        electionContract.setProvider(this.state.web3.currentProvider);
+        this.state.web3.eth.getAccounts((error, accounts) => {
+            electionContract.deployed().then((instance) => {
+                electionContractInstance = instance;
+                console.log(this.state.candidatesCount);
+                for (let i = 1; i <= this.state.candidatesCount; i++) {
+                    electionContractInstance.candidates.call(i)
+                        .then((result) => {
+                            this.setState(() => ({
+                                candidateArray: [...this.state.candidateArray,
+                                result[1]]
+                            }));
+                        })
+                }
+            })
         })
     }
 
@@ -61,10 +78,20 @@ class App extends Component {
                 </form>
                 {
                     this.state.candidatesCount ?
-                        (<h3>{this.state.candidatesCount} are registered.</h3>) :
+                        <h3>{this.state.candidatesCount} are registered.
+                        <button onClick={this.getAllCandidates} > Get All Candidates</button></h3> :
                         null
                 }
-            </div>
+                {
+                    this.state.candidateArray.map((candidate) => {
+                        return (
+                            <li key={Math.random()}>
+                                {this.state.web3.toAscii(candidate).replace(/\u0000/g, '')}
+                            </li>
+                        );
+                    })
+                }
+            </div >
         );
     }
 }
