7661fe49132515894e9796414ea33f416ea6f91d
Suzal3579 <pe.messh@gmail.com>
2018-03-03 13:46:46 +0530

    Setting up blackjack

--- dapp-project/src/components/App.js
+++ dapp-project/src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Vote from './voteapp/Vote';
 import RealEstate from './realestate/RealEstate';
+import BlackJack from './blackjack/BlackJack';
 import NotFound from './voteapp/NotFound';
 
 import '../css/App.css';
@@ -10,10 +11,11 @@ import '../css/App.css';
 class App extends Component {
     render() {
         return (
-              <BrowserRouter>
+            <BrowserRouter>
                 <Switch>
                     <Route exact path="/vote" component={Vote} />
                     <Route exact path="/realEstate" component={RealEstate} />
+                    <Route exact path="/blackJack" component={BlackJack} />
                     <Route component={NotFound} />
                 </Switch>
             </BrowserRouter >
---
+++ dapp-project/src/components/blackjack/BlackJack.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BlackJack = () => (
+    <div>
+        Bee Lack Jeez Yack ..
+    </div>
+);
+
+export default BlackJack;
