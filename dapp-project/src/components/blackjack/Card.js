2bb406154622fcf0688de1524a5451ba3768ff6a
Suzal3579 <pe.messh@gmail.com>
2018-03-05 14:25:48 +0530

    Images finaly working .... Uploading all images ... Plus workng code ... Layout fixing left ...

--- dapp-project/src/components/blackjack/Card.js
+++ dapp-project/src/components/blackjack/Card.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 
-import {
-    hidden,
-    back,
-    c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13,
-    d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13,
-    s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13,
-    h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11, h12, h13,
-} from '../imagesApi';
 
 class Card extends Component {
     render() {
+        const imageSource = `./img/${this.props.face}.png`;
         return (
-            <div>
+            <div >
                 {
                     this.props.hidden ?
-                        (<img src={hidden} alt="" />) :
-                        (<img src={this.props.face} alt="" />)
+                        (<img src="./img/hidden.png" alt="" />) :
+                        (<img src={imageSource} alt="" />)
                 }
-                {this.props.value}
             </div>
         );
     }
