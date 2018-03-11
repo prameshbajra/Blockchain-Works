f1aac3f8e211f5dd09b8d71295349dec7b7ca56b
Suzal3579 <pe.messh@gmail.com>
2018-03-11 23:23:30 +0530

    Updated test cases and updated solidity ...

--- dapp-project/contracts/Blackjack.sol
+++ dapp-project/contracts/Blackjack.sol
@@ -2,43 +2,68 @@ pragma solidity ^0.4.2;
 
 contract Blackjack {
 
-    uint256 bet;
-    address player;
-    address dealer;
-    uint256 payout;
-    
+    struct Game {
+        uint256 bet;
+        address player;
+        uint256 payout;
+        uint gameId;
+    }
+
+    mapping(uint => Game) public games;
+    mapping(address => uint256) currentGame;
+    mapping(address => uint256) cummulativePayout;
+    uint public gameCount;
+
     function Blackjack() payable {
-        dealer = msg.sender;
+        init(0);
+        gameCount = 0;
     }
 
     function () payable {
         startGame();
     }
-   
-    function init() private {
-        player = msg.sender;
-        bet = msg.value;
-        payout = 0;
-    }
-    
-    function endGame() private {
-        payout = bet*2;
-        closeGame();
+
+    function init(uint gameId) private {
+        games[gameId].player = msg.sender;
+        games[gameId].bet = msg.value;
+        games[gameId].payout = 0;
+        games[gameId].gameId = gameId;
     }
 
-    function closeGame() private {
-        if (payout > 0) {   
-            player.transfer(payout);
-            payout = 0;
-            bet = 0;
+    function addPayout(uint gameId) private {
+        if (games[gameId].payout == 0) {
+            games[gameId].payout = games[gameId].bet * 2;
+            games[gameId].bet = 0;
+            cummulativePayout[msg.sender] += games[gameId].payout;
         }
     }
-    
+
+    function closeGame(uint gameId) private {
+        if (cummulativePayout[msg.sender] > 0) {
+            games[gameId].player.transfer(cummulativePayout[msg.sender]);
+            cummulativePayout[msg.sender] = 0;
+        }
+    }
+
+    //call this function to set the bet, game Id, player
     function startGame() payable {
-         init();
+        uint256 newGameID = ++gameCount;
+        init(newGameID);
+        currentGame[msg.sender] = newGameID;
     }
-    
+
+    //Call this function on each win to increase cummulative cashout by bet X 2
+    function ifWin() {
+        addPayout(currentGame[msg.sender]);
+    }
+
+    //Cashout all money won by an account
     function redeem() {
-        endGame();
+        closeGame(currentGame[msg.sender]);
     }
+
+    function checkPayout() returns(uint256) {
+        return cummulativePayout[msg.sender];
+    }
+
 }
--- dapp-project/src/components/blackjack/Outcome.js
+++ dapp-project/src/components/blackjack/Outcome.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 
+import Blackjack from '../../../build/contracts/Blackjack.json';
+import getWeb3 from '../../utils/getWeb3';
+
 class Outcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            web3: null,
+        }
+    }
+    componentWillMount() {
+        getWeb3.then(results => {
+            this.setState({
+                web3: results.web3
+            })
+        }).catch(() => {
+            console.log('Error finding web3.')
+        })
+    }
     static defaultProps = {
         status: "playing"
     }
-    static status
+    ifWin = () => {
+        let blackjackContractInstance;
+        const contract = require('truffle-contract');
+        const blackJackContract = contract(Blackjack);
+        blackJackContract.setProvider(this.state.web3.currentProvider);
+        this.state.web3.eth.getAccounts((error, accounts) => {
+            blackJackContract.deployed().then((instance) => {
+                blackjackContractInstance = instance;
+                return blackjackContractInstance.ifWin({ from: accounts[0] });
+            }).then((result) => {
+                console.log("If win ko bata aako result", result);
+            })
+        });
+    }
     render() {
         switch (this.props.status) {
             case "playing":
-                return (<div className="alert alert-info" role="alert">Hit or Stand</div>);
+                return (<div className="alert alert-info" role="alert" > Hit or Stand</div>);
             case "win":
+                this.ifWin();
                 return (<div className="alert alert-success" role="alert">Win Win Win</div>);
             case "lose":
                 return (<div className="alert alert-danger" role="alert">You Lose</div>);
